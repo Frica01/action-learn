922fd096eb879b6d37cfc7dd3d8416aaaf441cbb
frica01 <819682099@qq.com>
2024-05-13 10:56:47 +0800

    flask

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+# 追加的代码
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
